9d2332786c65c77d5424413ced1cebc9fb9fac64
Oliver Electric <jamie.ostrowski@gmail.com>
2022-03-24 18:56:04 -0500

    Putting in a newline.

--- scratch/arrays.c
+++ scratch/arrays.c
@@ -12,11 +12,11 @@ int main(){
     strcpy (array1, "Hello World");
 
     p_element = &array1[0];
-    for (i=0; i<10; i++){
+    for (i=0; i<11; i++){
         putchar(*p_element);
         p_element++;
     }
-    
+    putchar('\n');
     return 0;
     
 }
