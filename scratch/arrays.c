b8e6413d889274d16287880ab51f652e0e735fef
Oliver Electric <jamie.ostrowski@gmail.com>
2022-03-27 13:02:10 -0500

    Pointer to pointer

--- scratch/arrays.c
+++ scratch/arrays.c
@@ -1,12 +1,15 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 
 int main(){
 
     char array1[20];
     char *p_element = array1;
+    char **pp_element = &p_element;
+
     int i;
 
     strcpy (array1, "Hello World");
@@ -14,9 +17,13 @@ int main(){
     p_element = &array1[0];
     for (i=0; i<11; i++){
         putchar(*p_element);
+        putchar(**pp_element);
         p_element++;
     }
     putchar('\n');
+
+
+
     return 0;
     
 }
