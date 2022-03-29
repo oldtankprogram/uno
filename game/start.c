35b917634cab55a704d38ce15bce4d01a726da4d
Oliver Electric <jamie.ostrowski@gmail.com>
2022-03-29 18:32:30 -0500

    4

--- game/start.c
+++ game/start.c
@@ -10,15 +10,24 @@ int main(){
     
     struct room { char roomName[20];
     char description[200];
+    struct room *p_next;
     };
     struct room *current;
+    struct room *orig;
 
     current = malloc(sizeof(struct room));
     strcpy (current->roomName, "Laboratory");
     strcpy (current->description, "Lab Room");
+    current->p_next = malloc(sizeof(struct room));
+    puts(current->roomName);
+
+    current = p_next;
+    strcpy (current->roomName, "Laboratory 2");
+    strcpy (current->description, "Another Lab Room");
 
     puts(current->roomName);
     free (current);
+    free (orig);
     return 0; 
 
 }
