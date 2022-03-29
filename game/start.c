11945702236150d9c6f34b8b3a580aded18dfef7
Oliver Electric <jamie.ostrowski@gmail.com>
2022-03-29 17:34:53 -0500

    Adding start.c

---
+++ game/start.c
@@ -0,0 +1,29 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+
+    struct room {char roomName[20];
+    char description[200];};
+    struct room *current;
+
+    current = malloc(sizeof(struct room));
+    strcpy (current->roomName, "Laboratory");
+    strcpy (current->description, "Lab Room");
+
+    puts(current->roomName);
+    free (current);
+    return 0;
+
+}
+
+
+
+
+
+
+
+}
