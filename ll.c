cfd6496a82f32af0895654366cb8e9e4101bdb6a
Oliver Electric <jamie.ostrowski@gmail.com>
2022-04-05 20:17:06 -0500

    Removing detritus.

--- ll.c
+++ ll.c
@@ -25,20 +25,16 @@ for (a = 0; a < 20; a++)
     pool[0] = calloc (1, sizeof(struct student));
     strncpy ( pool[0]->name, "Alfred", sizeof("Alfred") + 1);
     pool[0]->age = 21;
-    puts (pool[0]->name);
-    printf ("Age: %d\n", pool[0]->age);
+ 
 
     pool[1] = calloc (1, sizeof(struct student));
     strncpy ( pool[1]->name, "Brent", sizeof("Brent") + 1);
     pool[1]->age = 20;
-    puts (pool[1]->name);
-    printf ("Age: %d\n", pool[1]->age);
 
     pool[2] = calloc (1, sizeof(struct student));
     strncpy ( pool[2]->name, "Charlie", sizeof("Charlie") + 1);
     pool[2]->age = 22;
-    puts (pool[2]->name);
-    printf ("Age: %d\n", pool[2]->age);
+
 
 /* Traverse the pool, printing the data */
 
