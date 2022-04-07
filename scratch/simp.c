c00c23bf3e7521d73ac07f3481a4839896b90678
Oliver Electric <jamie.ostrowski@gmail.com>
2022-04-06 20:23:01 -0500

    Checking in simp.

---
+++ scratch/simp.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+int test(){
+
+    printf ("%d", a);
+    return 0;
+}
+
+int main() {
+
+    int a=1;
+
+    test();
+    return 0;
+}
