a07ef7020cf10b4f65100968b6efdc104a54292c
Oliver Electric <jamie.ostrowski@gmail.com>
2022-04-20 22:21:13 -0500

    Adding adding function.

--- scratch/filestruct.c
+++ scratch/filestruct.c
@@ -12,7 +12,9 @@ int main()
     FILE *fpout;
     char *buffer;
     size_t bufsiz = 300;
-    int one, two;
+    int one, two, collector;
+
+    collector = 0;
 
     fpin = fopen ("/home/jamie/input", "r");
     fpout = fopen ("/home/jamie/output", "r+");
@@ -30,8 +32,10 @@ int main()
     while (getline(&buffer, &bufsiz, fpin) > 0){
         sscanf(buffer, "%d %d", &one, &two);
         printf ("%d and %d\n", one, two);
+        collector = collector + one + two;
     }
 
+    printf ("The total amount of all the numbers is %d", collector);
  
     return 0;
     
